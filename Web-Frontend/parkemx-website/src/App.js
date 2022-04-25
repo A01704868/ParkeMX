b5ef288cd16c535d60da58d84003616a53d0a4ff
Ricardo Camarena <ricky_camarena@hotmail.com>
2022-04-25 17:14:10 -0500

    Progress in park view

--- Web-Frontend/parkemx-website/src/App.js
+++ Web-Frontend/parkemx-website/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component }  from 'react';
+import { Routes, Route, Link } from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" activeClassName="active" end="true">Here goes the logo</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="main">
+        {/* Define all the routes */}
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
     </div>
   );
+}
+
+export const Home = () => {
+  return (
+    <div className="hero-park">
+      <img src={require('./assets/header-placeholder.jpeg')}/>
+      <h1 className="h1"> NOMBRE DEL PARQUE</h1>
+    </div>
+    <div>
+    </div>
+  );
+}
+
+export const ParqueVista = () => {
+  return <div>You are in Parque View</div>
 }
 
 export default App;
