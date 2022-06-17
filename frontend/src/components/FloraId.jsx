cd16d39ad25e0825a88ba44485527982a9e1c688
Ricardo Camarena <ricky_camarena@hotmail.com>
2022-06-17 12:44:55 -0500

    Prepare vista flora y vista fauna para endpoints

--- frontend/src/components/FloraId.jsx
+++ frontend/src/components/FloraId.jsx
@@ -1,8 +1,71 @@
-import React, {useState} from 'react';
+import React from "react";
+import Navbar from "./BarraNav";
+import Footer from "./Footer";
+import "../css/styles.css";
+//import { useParams } from "react-router-dom";
+//import { Card, Button, Carousel, Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { Carousel, Container } from "react-bootstrap";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
-function FloraId(props) {
+function FloraId() {
+
+  const { id } = useParams();
+
+  const [flora, setFlora] = useState([]);
+
+  useEffect(() => {
+    const getData = () => {
+      let promise1 = axios.get(
+        "http://localhost:4000/api/parques/flora/" + id
+      );
+
+      Promise.all([promise1])
+      .then(values => {
+        setFlora(values[0].data);
+      })
+      .catch((e) => console.log(e));
+    };
+    getData();
+  }, []);
+  console.log(flora)
+
   return(
-    <h1> TEST </h1>
+    <div>
+      <Navbar />
+      <Container className="pb-4">
+        <h1>AQUI VA UNA IMAGEN QUE EL BUEN ALEX SE VA A RIFAR :D</h1>
+      <img
+        className="d-block w-100"
+        src= {flora.imagen}
+      />
+      </Container>
+      <h1>{/*flora.nombre*/}</h1>
+      <Container className="pb-4">
+        <h2>{/*flora.titulo*/}Albatrosses are the world's largest seabirds.
+        They spend at least 85% of their lives at sea returning
+        to land (usually remote islands) to breed and raise their young. SUSTITUIR POR TITULO DESC</h2>
+
+        <div className="row-info-card">
+          <div className="col-6">
+            <p className="mb-3">{/*flora.descripcion*/}Naturally low productivity, combined with changes in climate and habitat conditions and certain fishing practices, make these seabirds highly vulnerable.
+            Until recently scientists recognised 14 different species of albatrosses, but new research using DNA technology has confirmed as many as 24.
+            Thirteen varieties breed in the New Zealand region – more than anywhere else in the world.
+            Several are extremely rare, like the Antipodean albatross which breed in the sub Antarctic Island of Antipodes. </p>
+          </div>
+          <div className="col-6">
+            <h2 className="mb-3"> Acá va el carusel </h2>
+            <Carousel className="car-center">
+
+            </Carousel>
+          </div>
+        </div>
+      </Container>
+
+      <Footer />
+    </div>
   );
+
 }
 export default FloraId;
